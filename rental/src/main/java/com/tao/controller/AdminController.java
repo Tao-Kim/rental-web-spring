b696d04082fed71035b9a43249bcbe22e2bd6366
taoKim <taog397@gmail.com>
2020-05-07 23:08:51 +0900

    Add Admin page2

---
+++ rental/src/main/java/com/tao/controller/AdminController.java
@@ -0,0 +1,48 @@
+package com.tao.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@GetMapping("*")
+	public String home() {
+		return "index";
+	}
+	@GetMapping("/shop/list")
+	public String list() {
+		return "shop/list";
+	}
+	
+	@GetMapping("/shop/item")
+	public String item() {
+		return "shop/item";
+	}
+	@GetMapping("/shop/register")
+	public String register() {
+		return "shop/register";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
