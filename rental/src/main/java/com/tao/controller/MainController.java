e3cd758fb1ee800052aa0e140b84baeb6230173c
taoKim <taog397@gmail.com>
2020-05-08 00:47:39 +0900

    Backup 2020-05-08-00:47

--- rental/src/main/java/com/tao/controller/MainController.java
+++ rental/src/main/java/com/tao/controller/MainController.java
@@ -17,9 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class MainController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
